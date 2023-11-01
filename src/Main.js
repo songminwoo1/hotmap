d0adfd5f78e6c5d4a6e5a4e9ad6a1709d097bdf6
skysoon02 <skysoon02@kaist.ac.kr>
2023-11-01 22:25:19 +0900

    Button Work

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,30 @@
 import Map from "./Map";
 
+import { useState } from "react";
+
 import { Box, Button, Container } from "@mui/material";
 
-function TopUI(){
+function TopUI({age, setAge, gender, setGender}){
   return(
     <Container maxWidth={false} style={{position: 'absolute', padding: 0}} sx={{zIndex: 1500, width: 1, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
       <Box component="img" src="Logo.png"></Box>
       <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <Button variant ="contained" sx={{mr: 2, borderRadius: 30, bgcolor: 'white', ':hover':{bgcolor: '#EEEEEE'}, color: 'black', fontSize: 16}}>Under-Age</Button>
-        <Button variant ="contained" sx={{mr: 2, borderRadius: 30, bgcolor: 'white', ':hover':{bgcolor: '#EEEEEE'}, color: 'black', fontSize: 16}}>Adult</Button>
-        <Button variant ="contained" sx={{mr: 2, borderRadius: 30, bgcolor: 'white', ':hover':{bgcolor: '#EEEEEE'}, color: 'black', fontSize: 16}}>Men</Button>
-        <Button variant ="contained" sx={{mr: 2, borderRadius: 30, bgcolor: 'white', ':hover':{bgcolor: '#EEEEEE'}, color: 'black', fontSize: 16}}>Women</Button>
+        <Button variant ="contained" sx={{mr: 2, borderRadius: 30, bgcolor: age=='under-age' ? '#DDDDDD':'white', ':hover':{bgcolor: '#DDDDDD'}, color: 'black', fontSize: 16}} onClick={()=>{setAge('under-age')}}>Under-Age</Button>
+        <Button variant ="contained" sx={{mr: 2, borderRadius: 30, bgcolor: age=='adult' ? '#DDDDDD':'white', ':hover':{bgcolor: '#DDDDDD'}, color: 'black', fontSize: 16}} onClick={()=>{setAge('adult')}}>Adult</Button>
+        <Button variant ="contained" sx={{mr: 2, borderRadius: 30, bgcolor: gender=='men' ? '#DDDDDD':'white', ':hover':{bgcolor: '#DDDDDD'}, color: 'black', fontSize: 16}} onClick={()=>{setGender('men')}}>Men</Button>
+        <Button variant ="contained" sx={{mr: 2, borderRadius: 30, bgcolor: gender=='women' ? '#DDDDDD':'white', ':hover':{bgcolor: '#DDDDDD'}, color: 'black', fontSize: 16}} onClick={()=>{setGender('women')}}>Women</Button>
       </Box>
     </Container>
-  )
-  ;
+  );
 }
 
 function Main(){
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+
   return(
     <Container style={{minWidth: "100vw", width: "100vw", height: "100vh", padding: 0}} sx={{}}>
-      <TopUI></TopUI>
+      <TopUI age={age} setAge={setAge} gender={gender} setGender={setGender}></TopUI>
       <Map></Map>
     </Container>
   );
