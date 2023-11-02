d38e79a24b326c986eed942c884e7827b1a54c03
chanee718 <chanee718@kaist.ac.kr>
2023-11-03 05:24:46 +0900

    Add Pin

--- src/AddPlace.js
+++ src/AddPlace.js
@@ -3,17 +3,52 @@ import { closeSidebar } from "./slice";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { Box, Modal } from "@mui/material"
+import { Box, Button, IconButton, Modal, TextField } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import NotListedLocationIcon from '@mui/icons-material/NotListedLocation';
 
 function AddPlace() {
   const dispatch = useDispatch();
-  const  sidebar  = useSelector(state => state.sidebar.sidebarState);
+  const sidebar  = useSelector(state => state.sidebar.sidebarState);
 
   return (
-    <Modal open={sidebar==='addplace'} onClose={() => dispatch(closeSidebar())}>
-      <Box sx={{position: 'absolute', top: 0, right: 0, width: 400, height: 1, bgcolor: 'white'}}>
-        asdf
+    <Modal open={sidebar==='addplace'} onClose={() => dispatch(closeSidebar())}> 
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+      <Box 
+        sx={{
+          width: 300,
+          height: 300,
+          bgcolor: '#FFF4EC',
+          borderRadius: 2,
+          boxShadow: 3,
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',  // To align items vertically
+          alignItems: 'center',    // To center vertically
+        }} //outer box of Add Pin modal
+      >
+        <Box sx={{ display: 'flex', alignItems: 'flex-end' }} style={{ marginTop: '20px', marginRight: '20px' }}> 
+          <NotListedLocationIcon sx={{ color: '#FF6666', mr: 1, my: 0.5 }} />
+          <TextField id="entername" label="Name of Hot Place" variant="standard" /> 
+        </Box>  
+        <Box style={{ marginTop: '20px', marginLeft: '10px'}} >
+          <TextField 
+            helperText="Explain this Hot Place (Optional)"
+            label = "Explanation" 
+            variant = "outlined"
+            rows={4}
+            maxRows={4}
+            multiline
+          />
+        </Box>
+        <IconButton onClick={() => dispatch(closeSidebar())} style={{position: 'absolute', top: '10px', right: '10px'}}>
+          <CloseIcon />
+        </IconButton>
+        <Button variant="outlined" style={{position: 'absolute', bottom: '10px', right: '10px', color: '#FF6666', borderColor: '#FF6666'}}>
+          Add Pin
+        </Button>
       </Box>
+      </div>
     </Modal>
   )
 }
