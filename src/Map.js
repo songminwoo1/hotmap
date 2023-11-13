3824a2cb3c03879579e6a3e7f6d01a6c3197e0b2
skysoon02 <skysoon02@kaist.ac.kr>
2023-11-13 15:46:36 +0900

    stability of Map.js

--- src/Map.js
+++ src/Map.js
@@ -23,12 +23,13 @@ var tmp = [
 
 //https://4sii.tistory.com/424
 function Map({underage, adult, man, woman}){
-  const [places, setPlaces] = useState({'ID1': {'LatLng': {'_lat': 36.3954377, '_lng': 127.360221}, 'name': '한기원', 'stamp': {}}, 'ID2': {'LatLng': {'_lat': 36.3754377, '_lng': 127.355221}, 'name': '투썸', 'stamp': {}}});
+  const [places, setPlaces] = useState(null);
   const dispatch = useDispatch();
   const mapElement = useRef(null);
   const sidebarState = useSelector(state => state.sidebar.sidebarState);
   const text = useSelector(state => state.sidebar.text);
   const [temporaryLocation, setTemporaryLocation] = useState(null);
+
 
   const updatePinList = () => {
     GetPinList((new_pin_data) => {
@@ -38,16 +39,15 @@ function Map({underage, adult, man, woman}){
     })
   }
 
-  
 
   useEffect(() => {
-    //파이어베이스에서 데이터 받기S
-    GetPinList((data)=>setPlaces(data));
-    GetPinList((data)=>console.log(data));
-    
     if (!mapElement.current || !naver) return;
 
-    // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
+    //파이어베이스에서 데이터 받기
+    GetPinList((data)=>setPlaces(data));
+    GetPinList((data)=>console.log(data));
+
+    //지도 생성
     const location = new naver.maps.LatLng(36.3721427, 127.36039);
     const mapOptions = {
       center: location,
@@ -60,29 +60,9 @@ function Map({underage, adult, man, woman}){
       zoomControl: true,
       zoomControlOptions: { position: naver.maps.Position.LEFT_BOTTOM }
     };
-    console.log(places);
-
-    //지도 생성
     map = new naver.maps.Map(mapElement.current, mapOptions);
 
     //열지도 추가
-    for(var i=0; i<Object.keys(places).length; i++){
-      var weight = 0;
-      if(underage&&man){
-        weight+=places[Object.keys(places)[i]].stamp.UM;
-      }
-      if(underage&&woman){
-        weight+=places[Object.keys(places)[i]].stamp.UW;
-      }
-      if(adult&&man){
-        weight+=places[Object.keys(places)[i]].stamp.AM;
-      }
-      if(adult&&woman){
-        weight+=places[Object.keys(places)[i]].stamp.AW;
-      }
-      weights.push(new naver.maps.visualization.WeightedLocation(places[Object.keys(places)[i]].LatLng._lat, places[Object.keys(places)[i]].LatLng._lng, weight/100000));
-    }
-    
     naver.maps.onJSContentLoaded = function() {
       heatmap = new naver.maps.visualization.HeatMap({
           map: map,
@@ -91,68 +71,53 @@ function Map({underage, adult, man, woman}){
           radius: 15,
           colorMap: 'YIOrRd'
       });
+      
+      //zoom 정도에 따른 지도 변경
+      naver.maps.Event.addListener(map, 'zoom_changed', function(zoom) {
+        if(zoom<=16){ //show heat map
+          for(var i=0; i<markers.length; i++){
+            markers[i].setOptions({visible: false});
+          }
+          heatmap.setOptions('opacity', 1);
+          
+        }
+        else{ //show marker map
+          for(var i=0; i<markers.length; i++){
+            markers[i].setOptions({visible: true});
+          }
+          heatmap.setOptions('opacity', 0);
+        }
+      });
     };
-
-    //zoom 정도에 따른 지도 변경
-    naver.maps.Event.addListener(map, 'zoom_changed', function(zoom) {
-      if(zoom<=15){ //show heat map
-        for(var i=0; i<markers.length; i++){
-          markers[i].setOptions({visible: false});
-        }
-        heatmap.setOptions('opacity', 1);
-        
-      }
-      else{ //show marker map
-        for(var i=0; i<markers.length; i++){
-          markers[i].setOptions({visible: true});
-        }
-        heatmap.setOptions('opacity', 0);
-      }
-    });
-
-
+    
     //클릭 시 핫플 추가 창
     naver.maps.Event.addListener(map, 'click', (e) => {
       dispatch(readyAddPlace());
       setTemporaryLocation(e.coord);
     });
-
-    //사이드바를 불러오는 임시 이벤트 //삭제 필요
-    naver.maps.Event.addListener(map, 'keydown', function(e) {
-      var keyboardEvent = e.keyboardEvent,
-          keyCode = keyboardEvent.keyCode || keyboardEvent.which;
-
-
-      if (keyCode === 48) {
-          keyboardEvent.preventDefault();
-          dispatch(openWhiteboard());
-      }
-    });
-
   }, []);
 
+
+  //새로운 place 추가
   useEffect(() => {
     if(sidebarState === 'addplace' && temporaryLocation) {
-      // Add a marker at the temporarily saved location
-      const newmarker = new naver.maps.Marker({
-        position: temporaryLocation,
-        map,
-      });
-      // Change the state to 'none' after adding the pin
       AddPin({name: text, LatLng: temporaryLocation, stamp: {'UM': 0, 'UW': 0, 'AM': 0, 'AW': 0}}, updatePinList);
-      markers.push(newmarker);
       dispatch(closeSidebar());
       setTemporaryLocation(null); //db에서 가져와야 하는 데이터를 state로
     }
   }, [sidebarState, temporaryLocation]);
 
-  //마커 추가. 파이어 베이스에서 DB를 받은 후에 실행 됨
+  
+  //파이어 베이스에서 places를 받은 후에 실행 됨
   useEffect(() => {
+    if(places===null) return;
+
+    //markers 갱신
     console.log('places type: ', typeof(places));
     console.log('place length: ', Object.keys(places).length);
+    markers = [];
     var len = Object.keys(places).length;
     for(var i=0; i<len; i++){
-      if(Object.keys(places)[i].hasOwnProperty('data')) continue;  //추후 삭제 필요
       //마커 생성후 마커 리스트에 추가
       var marker = new naver.maps.Marker({
         position: places[Object.keys(places)[i]].LatLng,
@@ -164,13 +129,14 @@ function Map({underage, adult, man, woman}){
       function onClickEvent(i){
         return function(){
           dispatch(openWhiteboard());
-          dispatch(setLookingPlace(places[Object.keys(places)[i]]));
+          dispatch(setLookingPlace(Object.keys(places)[i]));
           dispatch(setLookingMarker(markers[i]));
         }
       };
       naver.maps.Event.addListener(marker, 'click', onClickEvent(i));
     }
-
+    
+    //weights 갱신
     if(heatmap===null) return;
     weights=[];
     for(var i=0; i<Object.keys(places).length; i++){
@@ -194,6 +160,7 @@ function Map({underage, adult, man, woman}){
     heatmap.redraw();
   }, [places]);
 
+  //버튼 눌리면 weights 갱신
   useEffect(() => {
     if(heatmap===null) return;
 
