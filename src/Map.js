4c6b30499d9e461f0e62530f1383e0c1adaba75d
skysoon02 <skysoon02@kaist.ac.kr>
2023-11-15 14:01:13 +0900

    marker icon and color

--- src/Map.js
+++ src/Map.js
@@ -21,6 +21,11 @@ function Map({underage, adult, man, woman}){
   const text = useSelector(state => state.sidebar.text);
   const [temporaryLocation, setTemporaryLocation] = useState(null);
 
+  function getMarkerIcon(weight, maxWeight){
+    return `<div style="font-size: 30px; color: rgb(255, ${102+(1-weight/maxWeight)*132}, ${102+(1-weight/maxWeight)*109})">
+              <i class="fa-solid fa-location-dot"></i>
+            </div>`
+  }
 
   const updatePinList = () => {
     GetPinList((new_pin_data) => {
@@ -85,40 +90,10 @@ function Map({underage, adult, man, woman}){
   useEffect(() => {
     if(places===null) return;
 
-    //markers 갱신
-    console.log('places type: ', typeof(places));
-    console.log('place length: ', Object.keys(places).length);
-
-    for(var i=0; i<markers.length; i++){
-      markers[i].setMap(null);
-    }
-    markers = [];
-    var zoom = map.getZoom();
-    var len = Object.keys(places).length;
-    for(var i=0; i<len; i++){
-      //마커 생성후 마커 리스트에 추가
-      var marker = new naver.maps.Marker({
-        position: places[Object.keys(places)[i]].LatLng,
-        map,
-        options: {visible: zoom<16 ? false : true}
-      });
-      //marker.setMap(map);
-      markers.push(marker);
-      function onClickEvent(i){
-        return function(){
-          dispatch(openWhiteboard());
-          dispatch(setLookingPlaceId(Object.keys(places)[i]));
-          dispatch(setLookingPlace(places[Object.keys(places)[i]]));
-          dispatch(setLookingMarker(markers[i]));
-        }
-      };
-      naver.maps.Event.addListener(marker, 'click', onClickEvent(i));
-    }
-    
-    //weights 갱신
+    //heatmap 갱신
     if(heatmap===null) return;
+    var maxWeight=0;
     weights=[];
-
     for(var i=0; i<Object.keys(places).length; i++){
       var weight = 1; //heatmap cognize weight=0 as none-weight, so weight=0 become max-weight
       if(underage&&man){
@@ -134,10 +109,43 @@ function Map({underage, adult, man, woman}){
         weight+=Object.keys(places[Object.keys(places)[i]].stamp.AW).length;
       }
       weights.push(new naver.maps.visualization.WeightedLocation(places[Object.keys(places)[i]].LatLng._lat, places[Object.keys(places)[i]].LatLng._lng, weight/100000));
+      
+      if(maxWeight<weight) maxWeight = weight;
     }
-
     heatmap.setData(weights);
     heatmap.redraw();
+
+    //markers 갱신
+    for(var i=0; i<markers.length; i++){
+      markers[i].setMap(null);
+    }
+    markers = [];
+    var zoom = map.getZoom();
+    for(var i=0; i<Object.keys(places).length; i++){
+      //마커 생성후 마커 리스트에 추가
+      var marker = new naver.maps.Marker({
+        position: places[Object.keys(places)[i]].LatLng,
+        map,
+        options: {visible: zoom<16 ? false : true},
+        icon: {
+          content: getMarkerIcon(weights[i].weight, maxWeight/100000),
+          size: new naver.maps.Size(22, 35),
+          anchor: new naver.maps.Point(11, 35)
+        }
+      });
+      markers.push(marker);
+      function onClickEvent(i){
+        return function(){
+          dispatch(openWhiteboard());
+          dispatch(setLookingPlaceId(Object.keys(places)[i]));
+          dispatch(setLookingPlace(places[Object.keys(places)[i]]));
+          dispatch(setLookingMarker(markers[i]));
+        }
+      };
+      naver.maps.Event.addListener(marker, 'click', onClickEvent(i));
+    }
+    
+    
 
     //zoom 정도에 따른 지도 변경
     naver.maps.Event.addListener(map, 'zoom_changed', function(zoom) {
